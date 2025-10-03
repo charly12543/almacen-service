4b3434b948c647f7060cb592e2784ec913755e8d
Carlos Bustamante <acuario12543@gmail.com>
2025-10-03 13:40:45 -0600

    Update mover-mouse.ps1

--- mover-mouse.ps1
+++ mover-mouse.ps1
@@ -1,4 +1,9 @@
 Set objShell = CreateObject("WScript.Shell")
+
+' Crear icono en bandeja con HTA
+Set objApp = CreateObject("Shell.Application")
+MsgBox "El script está activo. Para detenerlo, ciérralo desde el Administrador de tareas (wscript.exe).", vbInformation, "Teams Verde Activo"
+
 Do While True
     WScript.Sleep 30000 ' 30 segundos
     objShell.SendKeys("{SCROLLLOCK}")
